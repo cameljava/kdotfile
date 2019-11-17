dd10e698f3926b8f602b4ab5d052334b83d13cb6
Kevin Lee <cameljava@gmail.com>
2019-11-17 23:16:13 +1100

    add leader save quit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# backup vim and link user vimrc to my git
 mv ~/.vimrc $PWD/bak/vimrc_oldBak
 ln -s $PWD/vim/vimrc ~/.vimrc
+
+# backup .vim folder and link to my git
+mv ~/.vim $PWD/bak/vimFolder_oldBak
+ln -s $PWD/vim/dotVimFolder ~/.vim
